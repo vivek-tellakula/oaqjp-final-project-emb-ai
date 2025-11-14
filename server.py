d27be4ba3872ecbdeba7749e4a9073abc0194606
vivek-tellakula <keviv321@gmail.com>
2025-11-13 21:22:31 -0500

    part 8 commit

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-# Import necessary libraries and emotion_detector ()
+"""Flask app that exposes an /emotionDetector endpoint and a home page."""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,26 +6,25 @@ app = Flask("'Emotion Detector") # Instaniate Flask app
 
 @app.route("/emotionDetector")
 def emo_detector() :
-    # Retrieve the text to analyze from the request arguments
+    """Handle GET /emotionDetector, call emotion_detector(), and render a response."""
     text_to_analyse = request.args.get('textToAnalyze')
-    # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyse)
-    # Parse the resulting dictionary to seperate the emotions list and dominant emotion
     emotions = {k: v for k, v in response.items() if k != 'dominant_emotion'}
-    dominant = response["dominant_emotion"]
 
-    if response["dominant_emotion"] == None:
-        return ("Invalid text! Please try again!")
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+
+    dominant = response["dominant_emotion"].strip('"')
 
     emotions_str = ", ".join([f"'{k}': {v}" for k, v in emotions.items()])
     return (
-    "For the given statement, the system response is {}. "
-    "The dominant emotion is <b>{}</b>."
-    ).format(emotions_str, dominant.strip('"'))
+    f'For the given statement, the system response is {emotions_str}. '
+    f'The dominant emotion is <b>{dominant}</b>.'
+    )
 
-# Render template route
 @app.route("/")
 def render_index_page():
+    """Render the index page."""
     return render_template('index.html')
 
 # Set host/port information
